e72715c4c1df79e6984ae6f0d1789def8e84fbbb
Richa Yadav <richa.yadav.professional@gmail.com>
2024-12-27 19:17:15 -0500

    Time: 39 ms, Memory: 26.6 MB - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -7,8 +7,9 @@ class Solution:
         #     return False
         
         nums.sort()
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+
+        for i in range(0,len(nums) -1 ):
+            if nums[i] == nums[i+1]:
                 return True
         return False
         
