388a0e370b30ee5744f3ebb463476e17407894b8
Richa Yadav <richa.yadav.professional@gmail.com>
2025-01-02 16:53:30 -0500

    Time: 0 ms, Memory: 25.7 MB - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -3,9 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
         len_nums = len(nums)
         k = k%len_nums
+        ### Solution 1
+        # if k > 0 :
+        #     nums[:] = nums[-k:]+nums[:len_nums-k]
+        
+        ### Solution 2
         if k > 0 :
-            nums[:] = nums[-k:]+nums[:len_nums-k]
+            nums.reverse()
+            nums[0:k] = reversed(nums[0:k])
+            nums[k:] = reversed(nums[k:])
+            
+        
         
         
